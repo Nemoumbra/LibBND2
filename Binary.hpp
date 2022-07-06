86d6f1d5ebc8bc04f56f1079bb3ac5895b7d8b19
Nemoumbra <62447396+Nemoumbra@users.noreply.github.com>
2022-07-06 19:47:11 +0300

    Tabs -> spaces

--- Binary.hpp
+++ Binary.hpp
@@ -8,15 +8,15 @@
 
 class Binary {
 public:
-	//Binary();
-	static std::string get_file(const std::string& path);
-	static std::vector<unsigned char> file_to_uchar(const std::string& str);
-	static std::string uchar_to_file(std::vector<unsigned char>& data); // maybe const std::vector <uchar>& ?
-	static uint8_t get_uint8(const std::vector<unsigned char>& source, int offset);
-	static uint16_t get_uint16(const std::vector<unsigned char>& source, int offset);
-	static uint32_t get_uint32(const std::vector<unsigned char>& source, int offset);
-	static std::string get_string(const std::vector<unsigned char>& source, int offset);
-	static std::vector<unsigned char> get_block(const std::vector<unsigned char>& source, int offset, int block_size);
+    //Binary();
+    static std::string get_file(const std::string& path);
+    static std::vector<unsigned char> file_to_uchar(const std::string& str);
+    static std::string uchar_to_file(std::vector<unsigned char>& data); // maybe const std::vector <uchar>& ?
+    static uint8_t get_uint8(const std::vector<unsigned char>& source, int offset);
+    static uint16_t get_uint16(const std::vector<unsigned char>& source, int offset);
+    static uint32_t get_uint32(const std::vector<unsigned char>& source, int offset);
+    static std::string get_string(const std::vector<unsigned char>& source, int offset);
+    static std::vector<unsigned char> get_block(const std::vector<unsigned char>& source, int offset, int block_size);
 };
 
 #endif // BINARY_HPP
